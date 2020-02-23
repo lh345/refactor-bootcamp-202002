f8041820a489091fc4eb514c4f2cfdce3ca04fc5
Luo Huan <huan.luo@thoughtworks.com>
2020-02-23 23:23:22 +0800

    refactor: include testing for receipt style

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderReceiptTest {
     private final String DAY_IS_NOT_WEDNESDAY = "2020-02-18";
@@ -20,15 +21,20 @@ class OrderReceiptTest {
             add(new OrderItem("biscuits", 5.0, 5));
             add(new OrderItem("chocolate", 20.0, 1));
         }};
+        String expectedOutput = "===== 老王超市，值得信赖 ======\n" +
+                "\n" +
+                "2020年2月18日，星期二\n" +
+                "\n" +
+                "milk, 10.00 x 2, 20.00\n" +
+                "biscuits, 5.00 x 5, 25.00\n" +
+                "chocolate, 20.00 x 1, 20.00\n" +
+                "-----------------------------------\n" +
+                "税额: 6.50\n" +
+                "总价: 71.50\n";
         OrderReceipt receipt = new OrderReceipt(new Order(null, null, orderItems, LocalDate.parse(DAY_IS_NOT_WEDNESDAY)));
         String output = receipt.printReceipt();
 
-        assertThat(output, containsString("2020年2月18日，星期二"));
-        assertThat(output, containsString("milk, 10.00 x 2, 20.00"));
-        assertThat(output, containsString("biscuits, 5.00 x 5, 25.00"));
-        assertThat(output, containsString("chocolate, 20.00 x 1, 20.00"));
-        assertThat(output, containsString("税额: 6.50"));
-        assertThat(output, containsString("总价: 71.50"));
+        assertEquals(expectedOutput, output);
     }
 
     @Test
@@ -38,15 +44,20 @@ class OrderReceiptTest {
             add(new OrderItem("biscuits", 5.0, 5));
             add(new OrderItem("chocolate", 20.0, 1));
         }};
+        String expectedOutput = "===== 老王超市，值得信赖 ======\n" +
+                "\n" +
+                "2020年2月19日，星期三\n" +
+                "\n" +
+                "milk, 10.00 x 2, 20.00\n" +
+                "biscuits, 5.00 x 5, 25.00\n" +
+                "chocolate, 20.00 x 1, 20.00\n" +
+                "-----------------------------------\n" +
+                "税额: 6.50\n" +
+                "折扣: 1.43\n" +
+                "总价: 70.07\n";
         OrderReceipt receipt = new OrderReceipt(new Order(null, null, orderItems, LocalDate.parse(DAY_IS_WEDNESDAY)));
         String output = receipt.printReceipt();
 
-        assertThat(output, containsString("2020年2月19日，星期三"));
-        assertThat(output, containsString("milk, 10.00 x 2, 20.00"));
-        assertThat(output, containsString("biscuits, 5.00 x 5, 25.00"));
-        assertThat(output, containsString("chocolate, 20.00 x 1, 20.00"));
-        assertThat(output, containsString("税额: 6.50"));
-        assertThat(output, containsString("折扣: 1.43"));
-        assertThat(output, containsString("总价: 70.07"));
+        assertEquals(expectedOutput, output);
     }
 }
